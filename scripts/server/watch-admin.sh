a32b6076bcdfd0eafb7838f13ae5fecf55a101d7
bow-ph <131660069+bow-ph@users.noreply.github.com>
2025-02-13 13:38:46 +0100

    Delete scripts/server directory

--- scripts/server/watch-admin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ADMIN_DIR="/var/www/shopware6/custom/plugins/BOWAutoInternalLinks/src/Resources/app/administration"
-inotifywait -m -r -e modify,create,delete $ADMIN_DIR |
-while read path action file; do
-    if [[ "$file" =~ \.(js|vue|scss|css)$ ]]; then
-        echo "Changes detected in $path$file"
-        ./build-admin.sh
-    fi
-done
